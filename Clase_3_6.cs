dc4b2969a759446cd017b2b3e88fef9fd8dbf632
Ariel <96531908+elpoderoso1@users.noreply.github.com>
2025-07-28 22:54:02 -0600

    Crear Clase_3_6.cs

---
+++ Clase_3_6.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Class_III
+{
+    internal class Class_3_6
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la longitud del lado 1: ");
+            double lado1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la longitud del lado 2: ");
+            double lado2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la longitud del lado 3: ");
+            double lado3 = Convert.ToDouble(Console.ReadLine());
+
+            double perimetro = lado1 + lado2 + lado3;
+            Console.WriteLine("Perímetro del triángulo: " + perimetro);
+        }
+    }
+}
