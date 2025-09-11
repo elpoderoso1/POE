7e9c3cb180aede116d5ea50205d8db65612d39f0
Ariel <rodriargueta79@gmail.com>
2025-09-11 16:45:52 -0600

    Actual

---
+++ Clases/ProjectCS/System/System.Datas/Connection.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+
+namespace System.Datas
+{
+    public class Connection
+    {
+        private string DB;
+        private string SV;
+        private static Connection CON = null;
+        private Connection()
+        {
+            this.DB = "DBSystem";
+            this.SV = "localhost";
+        }
+        public SqlConnection CreateConnection()
+        {
+            SqlConnection Chain = new SqlConnection();
+            try
+            {
+                Chain.ConnectionString = $"Server={this.SV};Database={this.DB};Integrated Security=SSPI;";
+            }
+            catch (SqlException ex)
+            {
+                Chain = null;
+                throw ex;
+                throw new Exception(ex.Message);
+            }
+            return Chain;
+        }
+        public static Connection GetInstance()
+        {
+            if (CON == null)
+            {
+                CON = new Connection();
+            }
+            return CON;
+        }
+    }
+}
---
+++ Clases/ProjectCS/System/System.Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Entities
+{
+    public class Category
+    {
+        public int idcategoria { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public bool estado { get; set; }
+    }
+}
